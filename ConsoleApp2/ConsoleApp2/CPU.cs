50d6b665fb19d9cefd4bc020b067df66c1cfa775
ZaripovEgor <144524848+ZaripovEgor@users.noreply.github.com>
2024-11-22 17:20:38 +0500

    Review3

---
+++ ConsoleApp2/ConsoleApp2/CPU.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class CPU : Component, IInstallable
+    {
+        public double Frequency;
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Частота процессора: {Frequency} ГГц");
+        }
+        public void Install()
+        {
+            Console.WriteLine("Установка процессора в материнскую плату");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/GPU.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class GPU : Component, IInstallable
+    {
+
+        public int Volume;
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Объём видеопамяти: {Volume} ГБ");
+        }
+        public void Install()
+        {
+            Console.WriteLine("Установка видеокарты в материнскую плату");
+        }
+        
+    }
+}
