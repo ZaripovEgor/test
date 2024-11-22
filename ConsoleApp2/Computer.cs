053fd10a48b1c0c4e0b05600ebed725a1048a1c0
ZaripovEgor <144524848+ZaripovEgor@users.noreply.github.com>
2024-11-22 21:56:10 +0500

    Review3

---
+++ ConsoleApp2/Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Computer
+    {
+
+
+        public void Compare(Component component1, Component component2)
+        {
+            //
+        }
+    }
+}
---
+++ ConsoleApp2/RAM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class RAM : Component, IInstallable
+    {
+        public string Class;
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"Вид оперативной памяти: {Class}");
+        }
+        public void Install()
+        {
+            Console.WriteLine("Установка оперативной памти в материнскую плату");
+        }
+    }
+}
